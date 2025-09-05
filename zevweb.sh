534f2afafdf3de7211b68f72ad0e5f4c158ed9fe
2003Aditya <mishraaditya675@gmail.com>
2025-09-05 18:32:15 +0530

    updated backend url

--- zevweb.sh
+++ zevweb.sh
@@ -1,11 +1,11 @@
 !/bin/bash
 
-tmux new-session -d -s ZevWebOfficial
+tmux new-session -d -s ZevWebOfficial -c ~/Code/ZevFocusW
 
 tmux send-keys "nvim ~/Code/ZevFocusW" C-m
 tmux rename-window "Code"
 
-tmux new-window -t ZevWebOfficial:2 -n "term"
+tmux new-window -t ZevWebOfficial:2 -n "term" -c ~/Code/ZevFocusW
 tmux send-keys "nvim ~/Code/ZevFocusW -c 'terminal'" C-m
 
 tmux attach -t ZevWebOfficial
