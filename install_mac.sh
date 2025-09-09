413bfe24af351ff8fdaa1f4ab19afe0b41b0398b
2003Aditya <mishraaditya675@gmail.com>
2025-09-09 07:05:09 +0530

    update 1.2.4

--- install_mac.sh
+++ install_mac.sh
@@ -7,7 +7,7 @@ echo "📦 Installing ZevFocus CLI (macOS - Zsh)..."
 INSTALL_DIR="/usr/local/bin"
 BINARY_NAME="ZevFocusDarwin"
 TARGET_NAME="zevfocus"
-VERSION="1.2.3"
+VERSION="1.2.4"
 OS="darwin"
 
 # Ping API (non-blocking)
